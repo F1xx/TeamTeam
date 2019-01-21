8aa1af78f2d7f48aeed16cdfefa8e856b68e17da
Travis Britton <travinator41@gmail.com>
2019-01-21 16:39:56 -0500

    Created base Repo, project and contents

---
+++ Unreal Project/LootingLooters/Source/LootingLooters/LootingLootersGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LootingLootersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOOTINGLOOTERS_API ALootingLootersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Unreal Project/LootingLooters/Source/LootingLooters/Public/GuardCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "GuardCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOOTINGLOOTERS_API AGuardCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
